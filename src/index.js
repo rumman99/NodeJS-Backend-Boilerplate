c31f5a9cbcfd647a300b2ae691d78c853298d420
rumman99 <tasnimrumman369@gmail.com>
2024-04-16 22:04:56 +0600

    added apiError and response handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-// require('dotenv').config({path: './env'});
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({path: './.env'});
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Listening to PORT: ${process.env.PORT}`);
+    })
+})
+.catch(err=> {
+    console.log('Error in MongoDB Connection!!! ', err);
+})

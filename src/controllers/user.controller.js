d9f64ddce4fd2394740d83f5c3770ad4bb47990c
rumman99 <tasnimrumman369@gmail.com>
2024-04-20 06:13:33 +0600

    user login added in user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,17 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/AsyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
+const accessAndRefreshTokenGenerator= async(user)=>{
+    const accessToken= await user.generateAccessToken()
+    const refreshToken= await user.generateRefreshToken()
 
+    user.refreshToken=refreshToken;
+    await user.save({validateBeforeSave: false})
+
+    return {accessToken, refreshToken}
+}
+
+// REGISTRATION //
 const userRegister= asyncHandler(async(req, res)=>{
 
     const {username, email, fullName, password}= req.body;
@@ -57,4 +67,55 @@ const userRegister= asyncHandler(async(req, res)=>{
     }
 });
 
-export {userRegister};
+
+// LOGIN //
+const userLogin= asyncHandler(async(req, res)=>{
+    const {username, email, password}= req.body;
+
+    if(!username || !email || !password){
+        throw new ApiError(400, "All Field must be Required!!!");
+    }
+
+    const existedUser= await User.findOne({$or: [{username}, {email}]});
+
+    if(!existedUser){
+        throw new ApiError(404, "No User Found, Please Register First!!!");
+    }
+
+    const checkUserPassword= await existedUser.comparePassword(password);
+
+    if(!checkUserPassword){
+        throw new ApiError(401, "User Credential Not Match!!!");
+    }
+
+    const {accessToken, refreshToken}= await accessAndRefreshTokenGenerator(existedUser);
+
+    // Finding user with updated tokens //
+    const loggedInUser= await User.findOne(existedUser._id).select("-password -refreshToken")
+
+    // Cookie Options for Security //
+    const options= {
+        httpOnly: true,
+        secure: true
+    }
+
+    // Sending Response with status, cookie and login user Data //
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(
+                200, 
+                {
+                    user: loggedInUser, accessToken, refreshToken
+                }, 
+                "Login Successfully!!!"
+        ))
+})
+
+// LOGOUT //
+const logoutUser= asyncHandler(async(req, res)=> {
+
+})
+
+export {userRegister, userLogin, logoutUser};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { userRegister } from "../controllers/user.controller.js";
+import { userLogin, userRegister } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
 const router= Router();
 
 router.route('/register').post(upload.fields([{name: 'avatar', maxCount: 1}, {name: 'cover', maxCount: 1}]), userRegister) //added multer middleware//
+
+router.route('/login').post(userLogin)
 
 export default router;
